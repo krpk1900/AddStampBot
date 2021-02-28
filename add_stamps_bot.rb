f6f0c0dc407b828c1a8d5083bcc3f9ded37778ae
krpk1900 <krpk1900@gmail.com>
2021-02-28 13:19:05 +0900

    [Change] スタンプを押すチャンネルも複数指定できるように拡張

--- add_stamps_bot.rb
+++ add_stamps_bot.rb
@@ -6,21 +6,22 @@ end
 
 client = Slack::Web::Client.new
 # チャンネルIDを入れて使う
-channel = 'C01C9M24M0D'
+channels = ['C01JX3FMM9B', 'C010MLDEXJN', 'CL70X699R']
 
 stumps = ['+1', 'emoji_sugoi', 'emoji_otsukare', 'iine_', 'ileiilei', 'gannbare', 'daijoubu', 'wakaru', 'fire', 'ganbaro',
           'yoi', 'sorena', 'among_us', 'yokatta', 'tasikani', 'hayaine', 'yabatanienn', 'tuyotuyo', 'tasukete', 'kowaiyo',
           'yabasou', 'annoying_dog', 'nanikore']
 
-messages = client.conversations_history(channel: channel, limit: 10)['messages']
-for message in messages do
-  timestamp = message['ts']
-  stumps.shuffle!
-  for stump in stumps do
-    begin
-      client.reactions_add(channel: channel, name: stump, timestamp: timestamp)
-    rescue => e
+for channel in channels do
+  messages = client.conversations_history(channel: channel, limit: 10)['messages']
+  for message in messages do
+    timestamp = message['ts']
+    stumps.shuffle!
+    for stump in stumps do
+      begin
+        client.reactions_add(channel: channel, name: stump, timestamp: timestamp)
+      rescue => e
+      end
     end
   end
 end
-
